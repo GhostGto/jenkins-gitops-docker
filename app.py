7eee796bd638d90a7ae4836cffcce3eab6e30fd5
Sameera Dissanayaka <161738305+iam-sameera@users.noreply.github.com>
2024-03-16 01:50:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Flask is running in a Docker container! webhook test'
+    return 'Hello Sam'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
