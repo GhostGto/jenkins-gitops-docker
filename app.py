2dc6079ab3c9c5ce078d711ebdc568e1e63f04d2
Sameera Dissanayaka <161738305+iam-sameera@users.noreply.github.com>
2024-03-16 21:44:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Sam'
+    return 'Hello ArgoCD is Cool '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
